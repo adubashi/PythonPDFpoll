3158ee66697745fa08e814d6e23d77ad5d03c5ff
adubashi <adubashi@gmail.com>
2015-12-24 12:18:51 -0600

    added readme

--- pdfPollTest.py
+++ pdfPollTest.py
@@ -11,7 +11,8 @@ from pdfPoll import pdfPoll
 pdfPollTest = pdfPoll('bloombergPoll');
 pdfPollTest.poll.title = "Dubashi Strategies Iowa Poll"
 pdfPollTest.poll.studyNumber = 2145
-pdfPollTest.poll.addCandidate("Steve Jobs",[5,15,20]);
+pdfPollTest.poll.addCandidate("Steve Jobs",[5,75,80]);
 pdfPollTest.poll.addCandidate("Ben Carson",[20,10,30]);
-pdfPollTest.poll.addCandidate("Tom Harkin",[75,20,95]);
+pdfPollTest.poll.addCandidate("Tom Harkin",[65,5,70]);
+pdfPollTest.poll.addCandidate("Harry Enten",[10,10,20]);
 pdfPollTest.writeToPDF();
